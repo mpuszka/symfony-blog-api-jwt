89905928100c8e1af074454771342d7280eb2e10
Marcin Puszka <marcin.puszka@iamdripfeed.com>
2020-08-07 12:21:30 +0200

    add simply articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,17 +5,51 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Article;
+
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/article", name="article")
+     * @Route("/", name="articles")
      */
     public function index()
     {   
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $articles = $this->getDoctrine()
+                        ->getRepository(Article::class)
+                        ->findAll();
         
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="article")
+     */
+    public function article($id) 
+    {   
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+        $article = $this->getDoctrine()
+                        ->getRepository(Article::class)
+                        ->find($id);
+        
+        if (
+            !isset($article) || 
+            empty($article)
+        ) {
+            $this->addFlash(
+                'error',
+                'Article not found!'
+            );
+
+            return $this->redirectToRoute('articles');
+        }
+
+        return $this->render('article/article.html.twig', [
+            'article' => $article,
         ]);
     }
 }
