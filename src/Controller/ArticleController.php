6e1ae965f188af0502e722b922e951788875b9aa
Marcin Puszka <marcin.puszka@iamdripfeed.com>
2020-08-10 22:31:24 +0200

    add edit article form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Controller;
 
@@ -10,6 +11,7 @@ use App\Entity\Article;
 use App\Entity\Comment;
 
 use App\Form\Comment\CommentType;
+use App\Form\Article\ArticleType;
 
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -20,7 +22,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/", name="articles")
      */
-    public function index()
+    public function index(): object
     {   
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -36,7 +38,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article/{id}", name="article")
      */
-    public function article(Request $request, MessageBusInterface $bus, $id) 
+    public function article(Request $request, MessageBusInterface $bus, int $id): object
     {   
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
   
@@ -96,6 +98,57 @@ class ArticleController extends AbstractController
             'article'   => $article,
             'comments'  => $comments,
             'form'      => $form->createView(),
+        ]);
+    }
+
+    public function editArticle(Request $request, int $id): object 
+    {   
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+  
+        $article = $this->getDoctrine()
+                        ->getRepository(Article::class)
+                        ->find($id);
+        
+        if (
+            !isset($article) || 
+            empty($article)
+        ) {
+            $this->addFlash(
+                'error',
+                'Article not found!'
+            );
+
+            return $this->redirectToRoute('articles');
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articleRequest = $form->getData();
+            $entityManager  = $this->getDoctrine()->getManager();
+
+            $article->setTitle($articleRequest->getTitle());
+            $article->setBody($articleRequest->getBody());
+            $article->setImage($articleRequest->getImage());
+            $article->setAuthor($this->getUser());
+            $article->setDate(new \DateTime());
+            
+            $entityManager->persist($article);
+
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Article edited successfuly!'
+            );
+
+            return $this->redirectToRoute('article', ['id' => $id]);
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'article'   => $article,
+            'form'      => $form->createView()
         ]);
     }
 }
